6bcf4781d8ecad9d745bc13032878e403bc85acd
Adeel ur Rehman <adrehman@hotmail.com>
2024-05-07 00:27:31 +0500

    pulgin added check if loaded

--- OVS-Projects/ovs-plugin.js
+++ OVS-Projects/ovs-plugin.js
@@ -6,9 +6,19 @@ var lastPosition = {
 
 // Function to initialize the plugin
 function initializePlugin() {
-    // Draw plugin elements
-    drawPluginElements();
+    // Check if SIP.js is loaded
+    if (typeof SIP !== 'undefined' && SIP.UA) {
+        // SIP.js is loaded, call drawPluginElements()
+        drawPluginElements();
+    } else {
+        // SIP.js is not loaded, log an error
+        console.error('SIP.js is not loaded. Please make sure SIP.js is included before ovs-plugin.js.');
+    }
 }
+
+// Call initializePlugin() when the window finishes loading
+window.addEventListener('load', initializePlugin);
+
 
 // Function to draw the plugin elements
 function drawPluginElements() {
